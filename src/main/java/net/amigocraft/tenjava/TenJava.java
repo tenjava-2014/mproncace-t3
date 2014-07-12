7ac5cc89fbe6c08199887747412c4461cd88ac16
Maxim Roncace <mproncace@gmail.com>
2014-07-12 19:40:44 -0400

    Optimization, bug fixes, and biome manipulation

--- src/main/java/net/amigocraft/tenjava/TenJava.java
+++ src/main/java/net/amigocraft/tenjava/TenJava.java
@@ -15,6 +15,7 @@ import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.Material;
+import org.bukkit.block.Biome;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -28,11 +29,13 @@ public class TenJava extends JavaPlugin {
 	public static Material CORRUPT_BLOCK;
 	public static Material CLEANSE_BLOCK;
 	public static int CLEANSE_RADIUS;
+	public static boolean DEBUG;
 	public static Connection conn;
 	public static Statement st;
 	public static ResultSet rs;
 
-	public static List<Integer[]> corruptBlocks = new ArrayList<Integer[]>();
+	public static List<Object[]> corruptBlocks = new ArrayList<Object[]>();
+	public static List<Object[]> blockBuffer = new ArrayList<Object[]>();
 
 	public void onEnable(){
 
@@ -48,10 +51,11 @@ public class TenJava extends JavaPlugin {
 					Material.getMaterial(getConfig().getString("corrupt-block")) : Material.NETHERRACK;
 		}
 		{
-			CORRUPT_BLOCK = Material.getMaterial(getConfig().getString("cleanse-block")) != null ?
+			CLEANSE_BLOCK = Material.getMaterial(getConfig().getString("cleanse-block")) != null ?
 					Material.getMaterial(getConfig().getString("cleanse-block")) : Material.GLOWSTONE;
 		}
 		CLEANSE_RADIUS = getConfig().getInt("cleanse-radius");
+		DEBUG = getConfig().getBoolean("debug");
 
 		try {
 			Class.forName("org.sqlite.JDBC"); // to this day I still have no clue why this is necessary
@@ -71,10 +75,13 @@ public class TenJava extends JavaPlugin {
 					"id INTEGER NOT NULL PRIMARY KEY," +
 					"x INTEGER NOT NULL," +
 					"y INTEGER NOT NULL," +
-					"z INTEGER NOT NULL)");
+					"z INTEGER NOT NULL," +
+					"prevType VARCHAR NOT NULL," +
+					"prevBiome VARCHAR NOT NULL)");
 			rs = st.executeQuery("SELECT * FROM blocks");
 			while (rs.next()){
-				corruptBlocks.add(new Integer[]{rs.getInt("x"), rs.getInt("y"), rs.getInt("z")});
+				log.info("added");
+				corruptBlocks.add(new Object[]{rs.getInt("x"), rs.getInt("y"), rs.getInt("z"), rs.getString("prevType"), rs.getString("prevBiome")});
 			}
 		}
 		catch (SQLException | ClassNotFoundException ex){
@@ -106,6 +113,7 @@ public class TenJava extends JavaPlugin {
 		}
 		final List<Material> corruptTypes = corruptTypes1;
 		final List<Material> spreadTypes = spreadTypes1;
+		
 		Bukkit.getScheduler().runTaskTimer(plugin, new Runnable(){
 			public void run(){
 				Random r = new Random(); // now I'm automatically complying to the theme (*holds up spork*)
@@ -114,30 +122,33 @@ public class TenJava extends JavaPlugin {
 					Chunk chunk = chunks[r.nextInt(chunks.length)];
 					int x = r.nextInt(16);
 					int z = r.nextInt(16);
-					for (int y = 255; y > 0; y++){
+					for (int y = 255; y > 0; y--){
 						Block b = chunk.getBlock(x, y, z);
 						if (b.getType() != Material.AIR && (corruptTypes.contains(b.getType()))){
 							boolean protect = false;
-							for (int xx = -5; xx <= 5; xx++){
-								for (int yy = -5; yy <= 5; yy++){
-									for (int zz = -5; zz <= 5; zz++){
+							for (int xx = -CLEANSE_RADIUS; xx <= CLEANSE_RADIUS; xx++){
+								for (int yy = -CLEANSE_RADIUS; yy <= CLEANSE_RADIUS; yy++){
+									for (int zz = -CLEANSE_RADIUS; zz <= CLEANSE_RADIUS; zz++){
 										if (b.getWorld().getBlockAt(b.getX() + xx, b.getY() + yy, b.getZ() + zz).getType() == Material.GLOWSTONE)
 											protect = true;
 									}
 								}
 							}
 							if (!protect){
+								blockBuffer.add(new Object[]{b.getX(), b.getY(), b.getZ(), b.getType().toString(), b.getBiome().toString()});
+								corruptBlocks.add(new Object[]{b.getX(), b.getY(), b.getZ(), b.getType().toString(), b.getBiome().toString()});
 								b.setType(CORRUPT_BLOCK);
-								corruptBlocks.add(new Integer[]{b.getX(), b.getY(), b.getZ()});
+								b.setBiome(Biome.SKY);
+								log.info("Corrupted block randomly at " + b.getX() + ", " + b.getY() + ", " + b.getZ());
 							}
 							break;
 						}
 					}
 				}
-				List<Integer[]> buffer = new ArrayList<Integer[]>();
-				for (Integer[] ia : corruptBlocks){
+				List<Object[]> buffer = new ArrayList<Object[]>();
+				for (Object[] ia : corruptBlocks){
 					if (r.nextInt(SPREAD_CHANCE) == 0){
-						Block b = Bukkit.getWorlds().get(0).getBlockAt(ia[0], ia[1], ia[2]);
+						Block b = Bukkit.getWorlds().get(0).getBlockAt((int)ia[0], (int)ia[1], (int)ia[2]);
 						BlockFace[] faces = new BlockFace[]{BlockFace.NORTH, BlockFace.SOUTH, BlockFace.EAST, BlockFace.WEST};
 						Block bl = null;
 						int i = 0;
@@ -176,26 +187,34 @@ public class TenJava extends JavaPlugin {
 							}
 						}
 						if (bl != null){
+							buffer.add(new Object[]{bl.getX(), bl.getY(), bl.getZ(), bl.getType().toString(), bl.getBiome().toString()});
 							bl.setType(CORRUPT_BLOCK);
-							buffer.add(new Integer[]{bl.getX(), bl.getY(), bl.getZ()});
+							b.setBiome(Biome.SKY);
+							if (DEBUG)
+								log.info("Spread corruption to block at " + bl.getX() + ", " + bl.getY() + ", " + bl.getZ());
 						}
 					}
 				}
+				blockBuffer.addAll(buffer);
 				corruptBlocks.addAll(buffer);
 			}
 		}, 0L, 20L);
+		
+		log.info(this + " has been enabled! *holds up spork*");
+		
 	}
 
 	public void onDisable(){
-		if (corruptBlocks.size() > 0){
+		if (blockBuffer.size() > 0){
+			log.info("Saving corruptions...");
 			try {
 				Class.forName("org.sqlite.JDBC");
 				String dbPath = "jdbc:sqlite:" + getDataFolder() + File.separator + "data.db";
 				conn = DriverManager.getConnection(dbPath);
 				st = conn.createStatement();
-				for (Integer[] ia : corruptBlocks){
-					st.executeUpdate("INSERT INTO `blocks` (x, y, z) VALUES (" + ia[0] + ", " + ia[1] + ", " + ia[2] + ")");
-					System.out.println(ia[0] + ", " + ia[1] + ", " + ia[2]);
+				for (Object[] ia : blockBuffer){
+					st.executeUpdate("INSERT INTO `blocks` (x, y, z, prevType, prevBiome) VALUES (" + ia[0] + ", " + ia[1] + ", " + ia[2] + ", '" +
+					ia[3] + "', '" + ia[4] + "')");
 				}
 			}
 			catch (SQLException | ClassNotFoundException ex){
@@ -216,6 +235,9 @@ public class TenJava extends JavaPlugin {
 		rs = null;
 		st = null;
 		conn = null;
+		
+		log.info(this + " has been disabled! *puts down spork*");
+		
 		log = null;
 		plugin = null;
 	}
