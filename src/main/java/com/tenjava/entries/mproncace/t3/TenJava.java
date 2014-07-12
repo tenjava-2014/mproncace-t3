2e1ee186ce24f30e1423560eb9dc6fd282e2cb13
Maxim Roncace <mproncace@gmail.com>
2014-07-12 12:36:25 -0400

    Yay progress. :P

--- src/main/java/com/tenjava/entries/mproncace/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/mproncace/t3/TenJava.java
@@ -1,5 +1,22 @@
 package com.tenjava.entries.mproncace.t3;
 
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+	
+	public static TenJava plugin;
+	public static Logger log;
+	
+	public void onEnable(){
+		plugin = this;
+		log = getLogger();
+	}
+	
+	public void onDisable(){
+		log = null;
+		plugin = null;
+	}
+	
+}
