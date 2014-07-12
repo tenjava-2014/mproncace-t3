b395deb1a3d5a215cb54820e60ddb35b774313e5
Maxim Roncace <mproncace@gmail.com>
2014-07-12 18:31:10 -0400

    Optimize data storage (read: don't crash the server after 5 minutes)

--- src/main/java/net/amigocraft/tenjava/TenJava.java
+++ src/main/java/net/amigocraft/tenjava/TenJava.java
@@ -25,9 +25,14 @@ public class TenJava extends JavaPlugin {
 	public static Logger log;
 	public static int CORRUPT_CHANCE;
 	public static int SPREAD_CHANCE;
+	public static Material CORRUPT_BLOCK;
+	public static Material CLEANSE_BLOCK;
+	public static int CLEANSE_RADIUS;
 	public static Connection conn;
 	public static Statement st;
 	public static ResultSet rs;
+
+	public static List<Integer[]> corruptBlocks = new ArrayList<Integer[]>();
 
 	public void onEnable(){
 
@@ -38,22 +43,17 @@ public class TenJava extends JavaPlugin {
 
 		CORRUPT_CHANCE = getConfig().getInt("corruption-form-chance"); // for efficiency
 		SPREAD_CHANCE = getConfig().getInt("corruption-spread-chance");
+		{
+			CORRUPT_BLOCK = Material.getMaterial(getConfig().getString("corrupt-block")) != null ?
+					Material.getMaterial(getConfig().getString("corrupt-block")) : Material.NETHERRACK;
+		}
+		{
+			CORRUPT_BLOCK = Material.getMaterial(getConfig().getString("cleanse-block")) != null ?
+					Material.getMaterial(getConfig().getString("cleanse-block")) : Material.GLOWSTONE;
+		}
+		CLEANSE_RADIUS = getConfig().getInt("cleanse-radius");
 
 		try {
-			List<Material> corruptTypes1 = new ArrayList<Material>();
-			List<Material> spreadTypes1 = new ArrayList<Material>();
-			for (String m : getConfig().getStringList("allowed-form-types")){
-				Material mat = Material.getMaterial(m);
-				if (mat != null)
-					corruptTypes1.add(mat);
-			}
-			for (String m : getConfig().getStringList("allowed-spread-types")){
-				Material mat = Material.getMaterial(m);
-				if (mat != null)
-					spreadTypes1.add(mat);
-			}
-			final List<Material> corruptTypes = corruptTypes1;
-			final List<Material> spreadTypes = spreadTypes1;
 			Class.forName("org.sqlite.JDBC"); // to this day I still have no clue why this is necessary
 			String dbPath = "jdbc:sqlite:" + getDataFolder() + File.separator + "data.db";
 			if (!new File(getDataFolder(), "data.db").exists()){
@@ -62,6 +62,7 @@ public class TenJava extends JavaPlugin {
 				}
 				catch (IOException ex){
 					ex.printStackTrace();
+					log.severe("Failed to create database file!");
 				}
 			}
 			conn = DriverManager.getConnection(dbPath);
@@ -71,107 +72,146 @@ public class TenJava extends JavaPlugin {
 					"x INTEGER NOT NULL," +
 					"y INTEGER NOT NULL," +
 					"z INTEGER NOT NULL)");
-			Bukkit.getScheduler().runTaskTimer(plugin, new Runnable(){
-				public void run(){
-					List<Integer[]> corruptCoords = new ArrayList<Integer[]>(); // store newly
-					Random r = new Random(); // now I'm automatically complying to the theme :D
-					if (r.nextInt(CORRUPT_CHANCE) == 0){
-						Chunk[] chunks = Bukkit.getWorlds().get(0).getLoadedChunks();
-						Chunk chunk = chunks[r.nextInt(chunks.length)];
-						int x = r.nextInt(16);
-						int z = r.nextInt(16);
-						for (int y = 255; y > 0; y++){
-							Block b = chunk.getBlock(x, y, z);
-							if (b.getType() != Material.AIR && (corruptTypes.contains(b.getType()))){
-								boolean protect = false;
-								for (int xx = -5; xx <= 5; xx++){
-									for (int yy = -5; yy <= 5; yy++){
-										for (int zz = -5; zz <= 5; zz++){
-											if (b.getWorld().getBlockAt(b.getX() + xx, b.getY() + yy, b.getZ() + zz).getType() == Material.GLOWSTONE)
-												protect = true;
-										}
-									}
-								}
-								if (!protect){
-									b.setType(Material.NETHERRACK);
-									corruptCoords.add(new Integer[]{b.getX(), b.getY(), b.getZ()});
-								}
-								break;
-							}
-						}
-					}
-					try {
-						rs = st.executeQuery("SELECT * FROM blocks");
-						while (rs.next()){
-							if (r.nextInt(SPREAD_CHANCE) == 0){
-								Block b = Bukkit.getWorlds().get(0).getBlockAt(rs.getInt("x"), rs.getInt("y"), rs.getInt("z"));
-								BlockFace[] faces = new BlockFace[]{BlockFace.NORTH, BlockFace.SOUTH, BlockFace.EAST, BlockFace.WEST};
-								Block bl = null;
-								while (bl == null){
-									BlockFace face = faces[r.nextInt(4)];
-									Block tb = b.getRelative(face);
-									if (tb.getType() == Material.AIR){
-										for (int y = b.getY(); y > 0; y--){
-											if (tb.getWorld().getBlockAt(tb.getX(), y, tb.getZ()).getType() != Material.AIR &&
-													(corruptTypes.contains(b.getType()))){
-												boolean protect = false;
-												for (int xx = -5; xx <= 5; xx++){
-													for (int yy = -5; yy <= 5; yy++){
-														for (int zz = -5; zz <= 5; zz++){
-															if (b.getWorld().getBlockAt(tb.getX() + xx, tb.getY() + yy,
-																	tb.getZ() + zz).getType() == Material.GLOWSTONE)
-																protect = true;
-														}
-													}
-												}
-												if (!protect){
-													bl = tb.getWorld().getBlockAt(tb.getX(), y, tb.getZ());
-													break;
-												}
-											}
-										}
-									}
-									else { // I mean, yeah, I could use bitwise, but I don't really *feel* like it
-										for (int yy = b.getY(); yy < 256; yy++){
-											if (tb.getWorld().getBlockAt(tb.getX(), yy, tb.getZ()).getType() == Material.AIR){
-												bl = tb.getWorld().getBlockAt(tb.getX(), yy- 1, tb.getZ());
-												break;
-											}
-										}
-									}
-								}
-								bl.setType(Material.NETHERRACK);
-								corruptCoords.add(new Integer[]{bl.getX(), bl.getY(), bl.getZ()});
-							}
-						}
-						if (corruptCoords.size() > 0){
-							for (Integer[] ia : corruptCoords){
-								st.executeUpdate("INSERT INTO `blocks` (x, y, z) VALUES (" + ia[0] + ", " + ia[1] + ", " + ia[2] + ")");
-								System.out.println(ia[0] + ", " + ia[1] + ", " + ia[2]);
-							}
-						}
-					}
-					catch (SQLException ex){
-						ex.printStackTrace();
-					}
-				}
-			}, 0L, 20L);
+			rs = st.executeQuery("SELECT * FROM blocks");
+			while (rs.next()){
+				corruptBlocks.add(new Integer[]{rs.getInt("x"), rs.getInt("y"), rs.getInt("z")});
+			}
 		}
 		catch (SQLException | ClassNotFoundException ex){
 			ex.printStackTrace();
+			log.severe("Failed to create database!");
 		}
-
+		finally {
+			try {
+				rs.close();
+				st.close();
+				conn.close();
+			}
+			catch (SQLException ex){
+				ex.printStackTrace();
+				log.severe("Failed to close the database connection!");
+			}
+		}
+		List<Material> corruptTypes1 = new ArrayList<Material>();
+		List<Material> spreadTypes1 = new ArrayList<Material>();
+		for (String m : getConfig().getStringList("allowed-form-types")){
+			Material mat = Material.getMaterial(m);
+			if (mat != null)
+				corruptTypes1.add(mat);
+		}
+		for (String m : getConfig().getStringList("allowed-spread-types")){
+			Material mat = Material.getMaterial(m);
+			if (mat != null)
+				spreadTypes1.add(mat);
+		}
+		final List<Material> corruptTypes = corruptTypes1;
+		final List<Material> spreadTypes = spreadTypes1;
+		Bukkit.getScheduler().runTaskTimer(plugin, new Runnable(){
+			public void run(){
+				Random r = new Random(); // now I'm automatically complying to the theme (*holds up spork*)
+				if (r.nextInt(CORRUPT_CHANCE) == 0){
+					Chunk[] chunks = Bukkit.getWorlds().get(0).getLoadedChunks();
+					Chunk chunk = chunks[r.nextInt(chunks.length)];
+					int x = r.nextInt(16);
+					int z = r.nextInt(16);
+					for (int y = 255; y > 0; y++){
+						Block b = chunk.getBlock(x, y, z);
+						if (b.getType() != Material.AIR && (corruptTypes.contains(b.getType()))){
+							boolean protect = false;
+							for (int xx = -5; xx <= 5; xx++){
+								for (int yy = -5; yy <= 5; yy++){
+									for (int zz = -5; zz <= 5; zz++){
+										if (b.getWorld().getBlockAt(b.getX() + xx, b.getY() + yy, b.getZ() + zz).getType() == Material.GLOWSTONE)
+											protect = true;
+									}
+								}
+							}
+							if (!protect){
+								b.setType(CORRUPT_BLOCK);
+								corruptBlocks.add(new Integer[]{b.getX(), b.getY(), b.getZ()});
+							}
+							break;
+						}
+					}
+				}
+				List<Integer[]> buffer = new ArrayList<Integer[]>();
+				for (Integer[] ia : corruptBlocks){
+					if (r.nextInt(SPREAD_CHANCE) == 0){
+						Block b = Bukkit.getWorlds().get(0).getBlockAt(ia[0], ia[1], ia[2]);
+						BlockFace[] faces = new BlockFace[]{BlockFace.NORTH, BlockFace.SOUTH, BlockFace.EAST, BlockFace.WEST};
+						Block bl = null;
+						int i = 0;
+						while (bl == null){
+							if (i >= 10)
+								break; // to prevent an infinite loop
+							i += 1;
+							BlockFace face = faces[r.nextInt(4)];
+							Block tb = b.getRelative(face);
+							for (int y = b.getY(); tb.getType() == Material.AIR ? y > 0 : y < 255; y += (tb.getType() == Material.AIR ? -1 : 1)){
+								if (tb.getWorld().getBlockAt(tb.getX(), y, tb.getZ()).getType() != Material.AIR &&
+										(spreadTypes.contains(tb.getType()))){
+									boolean protect = false;
+									for (int xx = -CLEANSE_RADIUS; xx <= CLEANSE_RADIUS; xx++){
+										for (int yy = -CLEANSE_RADIUS; yy <= CLEANSE_RADIUS; yy++){
+											for (int zz = -CLEANSE_RADIUS; zz <= CLEANSE_RADIUS; zz++){
+												if (b.getWorld().getBlockAt(tb.getX() + xx, tb.getY() + yy,
+														tb.getZ() + zz).getType() == CLEANSE_BLOCK){
+													protect = true;
+													break;
+												}
+											}
+											if (protect)
+												break;
+										}
+										if (protect)
+											break;
+									}
+									if (!protect){
+										bl = tb.getWorld().getBlockAt(tb.getX(), y, tb.getZ());
+										break;
+									}
+									else
+										break;
+								}
+							}
+						}
+						if (bl != null){
+							bl.setType(CORRUPT_BLOCK);
+							buffer.add(new Integer[]{bl.getX(), bl.getY(), bl.getZ()});
+						}
+					}
+				}
+				corruptBlocks.addAll(buffer);
+			}
+		}, 0L, 20L);
 	}
 
 	public void onDisable(){
-		try {
-			rs.close();
-			st.close();
-			conn.close();
-		}
-		catch (Exception ex){
-			ex.printStackTrace();
-			log.severe("Failed to close database connection!");
+		if (corruptBlocks.size() > 0){
+			try {
+				Class.forName("org.sqlite.JDBC");
+				String dbPath = "jdbc:sqlite:" + getDataFolder() + File.separator + "data.db";
+				conn = DriverManager.getConnection(dbPath);
+				st = conn.createStatement();
+				for (Integer[] ia : corruptBlocks){
+					st.executeUpdate("INSERT INTO `blocks` (x, y, z) VALUES (" + ia[0] + ", " + ia[1] + ", " + ia[2] + ")");
+					System.out.println(ia[0] + ", " + ia[1] + ", " + ia[2]);
+				}
+			}
+			catch (SQLException | ClassNotFoundException ex){
+				ex.printStackTrace();
+			}
+			finally {
+				try {
+					rs.close();
+					st.close();
+					conn.close();
+				}
+				catch (Exception ex){
+					ex.printStackTrace();
+					log.severe("Failed to close database connection!");
+				}
+			}
 		}
 		rs = null;
 		st = null;
